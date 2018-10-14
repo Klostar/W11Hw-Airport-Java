b9301768fb36976a2db9604f47fdf8c63f7d083f
chloe alcorn <ca.alcorn@outlook.com>
2018-10-14 18:48:08 +0100

    flight has flight number

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,18 @@
 public class Flight {
+
+    private String flightNumber;
+    private PlaneType planeType;
+    private String destination;
+
+    public Flight(String flightNumber, PlaneType planeType, String destination){
+        this.flightNumber = flightNumber;
+        this.planeType = planeType;
+        this.destination = destination;
+
+    }
+
+
+    public String hasFlightNumber() {
+        return this.flightNumber;
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+    Flight flight;
+
+    @Before
+    public void before(){
+        flight = new Flight("FR589",PlaneType.BOEING767,"Pisa");
+    }
+
+    @Test
+    public void flightHasFlightNumber(){
+        assertEquals("FR589", flight.hasFlightNumber());
+    }
 }
