5296664535cafc06f7fb5909ceb8d66e8e6a414b
chloe alcorn <ca.alcorn@outlook.com>
2018-10-14 19:19:30 +0100

    has airport code

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Airport {
+    private String airportCode;
+    private ArrayList<Plane>hanger;
+
+    public Airport(String airportCode){
+        this.airportCode = airportCode;
+        this.hanger = new ArrayList<>();
+
+    }
+
+    public String hasAirportCode() {
+       return this.airportCode;
+
+    }
 }
--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class AirportTest {
+    Airport airport;
+    Plane plane;
+    Flight flight;
+
+
+    @Before
+    public void before(){
+        airport = new Airport("GLA");
+        plane = new Plane("Ryan Air", PlaneType.BOEING787);
+        flight = new Flight("FR543", PlaneType.BOEING767,"Rome");
+    }
+
+    @Test
+    public void hasAirportCode(){
+        assertEquals("GLA", airport.hasAirportCode());
+    }
+
+
+
 }
